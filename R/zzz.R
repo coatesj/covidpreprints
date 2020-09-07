9c2b51cea959dc6a7c5abcc30268f85e7470bb2a
Hugo Gruson <hugo.gruson@protonmail.com>
2020-09-07 20:54:09 +0200

    Move all the work to the vignette itself

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-
-  # Set Google de-authentication mode as sheet is public
-  # N.B. Authentication will be required if sheet is made private
-  googlesheets4::gs4_deauth()
-
-}
