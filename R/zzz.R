81ab991e53125e383b725aefbf7e2cc0bd33c13d
Hugo Gruson <hugo.gruson@protonmail.com>
2020-09-07 16:01:25 +0200

    Run devtools::document()

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,7 @@
-#' @importFrom googlesheets4 gs4_deauth
 .onLoad <- function(libname, pkgname) {
 
   # Set Google de-authentication mode as sheet is public
   # N.B. Authentication will be required if sheet is made private
-  gs4_deauth()
+  googlesheets4::gs4_deauth()
 
 }
