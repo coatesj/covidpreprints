751e6c6b1e4c6275dac45901829ba23aa20a67c3
Hugo Gruson <hugo.gruson@protonmail.com>
2020-09-07 15:58:54 +0200

    Move json updating code to its own function

--- R/script.R
+++ R/script.R
@@ -6,9 +6,6 @@
 #' @import httr
 #' @import jsonlite
 
-# Set Google Sheet URL
-sheet_url <- "https://docs.google.com/spreadsheets/d/1BES52D4nYUZjV6kx1S_lJKSHvC9I0uzPTFA2mcxv4FI"
-
 # Function to get various info from Europe PMC
 epmc <- function(doi){
   data <- epmc_search_by_doi(doi = doi,
@@ -45,27 +42,3 @@ altmetric <- function(doi){
                 altmetricDonut = data$images.small,
                 altmetricUrl = data$details_url))
 }
-
-# Read Google Sheet (Preprints tab) and add data
-preprints <- read_sheet(sheet_url, sheet = "Preprints") %>%
-  filter(class %in% c("preprint", "bad")) %>% # filter non-preprints
-  filter(!str_detect(id, "arXiv")) %>% # filter arXiv preprints (for now...)
-  mutate(publication = map(id, epmc),
-         altmetric = map(id, altmetric),
-         url = str_c("https://doi.org/", id)) %>%
-  unnest(publication, altmetric)
-
-# Read Google Sheet (Events tab)
-events <- read_sheet(sheet_url, sheet = "Events") %>%
-  filter(class %in% c("event"))
-
-# Combine and write to JSON object
-combined <- bind_rows(preprints, events) %>%
-  select(date, class, title, url,
-         briefSummary, longerSummary,
-         authorString, publishedVersionDoi, citationCount,
-         altmetricScore, altmetricUrl, altmetricDonut, tweeters,
-         preLightsUrl
-         ) %>%
-  arrange(date) %>%
-  write_json(., "data.json", pretty = TRUE)
---
+++ R/update_covidpreprints_json.R
@@ -0,0 +1,27 @@
+update_covidpreprints_json <- function(sheet_url) {
+
+  # Read Google Sheet (Preprints tab) and add data
+  preprints <- read_sheet(sheet_url, sheet = "Preprints") %>%
+    filter(class %in% c("preprint", "bad")) %>% # filter non-preprints
+    filter(!str_detect(id, "arXiv")) %>% # filter arXiv preprints (for now...)
+    mutate(publication = map(id, epmc),
+           altmetric = map(id, altmetric),
+           url = str_c("https://doi.org/", id)) %>%
+    unnest(publication, altmetric)
+
+  # Read Google Sheet (Events tab)
+  events <- read_sheet(sheet_url, sheet = "Events") %>%
+    filter(class %in% c("event"))
+
+  # Combine and write to JSON object
+  combined <- bind_rows(preprints, events) %>%
+    select(date, class, title, url,
+           briefSummary, longerSummary,
+           authorString, publishedVersionDoi, citationCount,
+           altmetricScore, altmetricUrl, altmetricDonut, tweeters,
+           preLightsUrl
+    ) %>%
+    arrange(date) %>%
+    write_json(., "data.json", pretty = TRUE)
+
+}
